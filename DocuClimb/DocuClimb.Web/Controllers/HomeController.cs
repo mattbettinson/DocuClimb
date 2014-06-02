2c3aa62e050c64be3f65e5b549346f171087552e
Matthew Bettinson <matthew.bettinson@oakbrookfinance.com>
2014-06-02 15:31:55 +0100

    Revert "Recreating DocuClimb.Web with authentication linked to azure ACS."
    This reverts commit a69421d7a591126e47788a1f2edfbf31dcb297c7.

--- DocuClimb/DocuClimb.Web/Controllers/HomeController.cs
+++ DocuClimb/DocuClimb.Web/Controllers/HomeController.cs
@@ -8,23 +8,11 @@ namespace DocuClimb.Web.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Site/
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+	}
 }
--- DocuClimb/DocuClimb.Web/Properties/AssemblyInfo.cs
+++ DocuClimb/DocuClimb.Web/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DocuClimb.Web")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("749f8533-e672-43d2-b9af-c665c298b996")]
+[assembly: Guid("411329ed-7417-495a-aa81-c0a92aa14fc9")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
